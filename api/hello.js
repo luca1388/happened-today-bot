9ef4cbd88420d2ab386d471f28d03af5eba0e71c
luca <luca.gandini88@gmail.com>
2024-10-18 16:14:59 +0200

    feat: Add logs

--- api/hello.js
+++ api/hello.js
@@ -11,5 +11,7 @@ export async function GET(request) {
 
   const data = await response.json();
 
+  console.log(data.parse.text.replace(/<[^>]*>/g, ""));
+
   return new Response(`Hello ${data.parse.text.replace(/<[^>]*>/g, "")}`);
 }
