88d89118f261f274ca11b33fc570b9a01ac8ea7e
luca <luca.gandini88@gmail.com>
2024-10-18 16:07:21 +0200

    feat: Basic fetch request

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,15 @@
-export function GET(request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+export async function GET(request) {
+  const today = new Date()
+    .toLocaleString("en-EN", { month: "long", day: "numeric" })
+    .replace(" ", "_");
+
+  console.log(today);
+
+  const response = await fetch(
+    `https://en.wikipedia.org/w/api.php?action=parse&page=${today}&format=json&prop=text%7Clanglinks%7Ccategories%7Clinks%7Ctemplates%7Cimages%7Cexternallinks%7Csections%7Crevid%7Cdisplaytitle%7Ciwlinks%7Cproperties%7Cparsewarnings&formatversion=2`
+  );
+
+  const data = await response.json();
+
+  return new Response(`Hello ${data.parse.text.replace(/<[^>]*>/g, "")}`);
 }
