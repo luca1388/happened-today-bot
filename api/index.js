a13bcc4de9bab29586f86150672bda23831d7142
luca <luca.gandini88@gmail.com>
2024-10-18 15:43:58 +0200

    feat: Dummy function

--- api/index.js
+++ api/index.js
@@ -1,8 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+export function GET(request) {
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+}
